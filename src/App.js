41c44957e20f6e6a1b08f55090371b4f477e41ac
RabibJamil <54376645+RabibJamil@users.noreply.github.com>
2024-10-05 15:07:54 -0400

    update homepage url

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { HashRouter as Router } from 'react-router-dom'; // Import HashRouter
 import AmortizationCalculator from './AmortizationCalculator';
 import './App.css';
 
 const App = () => {
     return (
+      <Router> {/* Wrap your app in HashRouter */}
         <div className="App">
             <AmortizationCalculator />
         </div>
+    </Router>
     );
 };
 
